68ea995f379859c368e85830f434591aad8ec337
PowerfulCat <x4495@outlook.com>
2019-10-21 14:37:16 +0800

    Add two simple example for use this library.

---
+++ example/hello_world/hello_world.ino
@@ -0,0 +1,11 @@
+#include<grove_terminal.h>
+
+grove_terminal terminal;
+void setup(){
+    terminal.begin();
+    terminal.println("Hello world!");
+}
+
+void loop(){
+    
+}
---
+++ example/loop_print/loop_print.ino
@@ -0,0 +1,17 @@
+#include<grove_terminal.h>
+#define TFT_WHITE   0xffff
+#define TFT_BLUE    0x001f
+
+grove_terminal terminal;
+int32_t        value = 0;
+void setup(){
+    terminal.begin();
+    terminal.foreground(TFT_WHITE);
+    terminal.background(TFT_BLUE);
+}
+
+void loop(){
+    terminal.println(value);
+    value += 1;
+    delay(1000);
+}
